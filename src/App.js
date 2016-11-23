396aba1d844bf57c05285c112d2b6e56c7dbdbf2
Marco Worms <marcogworms@gmail.com>
2016-11-23 00:46:17 -0200

    Add graph component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div style={{
-          boxSizing: 'border-box',
-          float: 'left',
-          width: '50%',
-          height: '50%',
-          border: 'solid 1px black'
-        }} />
-        <div style={{
-          boxSizing: 'border-box',
-          float: 'left',
-          width: '50%',
-          height: '50%',
-          border: 'solid 1px black'
-        }} />
-        <div style={{
-          boxSizing: 'border-box',
-          float: 'left',
-          width: '50%',
-          height: '50%',
-          border: 'solid 1px black'
-        }} />
-        <div style={{
-          boxSizing: 'border-box',
-          float: 'left',
-          width: '50%',
-          height: '50%',
-          border: 'solid 1px black'
-        }} />
-      </div>
-    );
-  }
+function debug (x) {
+  console.log(x)
+  return x
 }
 
-export default App;
+const Quarter = ({ children }) =>
+  <div
+    style={{
+      boxSizing: 'border-box',
+      float: 'left',
+      width: '50%',
+      height: '50%',
+      border: 'solid 1px black'
+    }}
+  >
+    { children }
+  </div>
+
+const Point = ({ x, y, size }) => {
+  size = size || 8
+  const normalize = (coord) => coord - size/2 - 1
+  return (
+    <div
+      style={{
+        width: size + 'px',
+        height: size + 'px',
+        borderRadius: '100%',
+        backgroundColor: 'red',
+        position: 'absolute',
+        transform: `translate(${ normalize(x) }px, ${ normalize(y) }px)`
+      }}
+    />
+  )
+}
+
+
+const Formula = ({ xCoords, f }) =>
+  <div>
+    {xCoords.map( (x, index) =>
+      <Point x={x} y={-f(x)} key={index} />
+    )}
+  </div>
+
+
+const buildRange = size =>
+  Array(size).fill(1).map((e,i,a)=>i-a.length/2)
+
+const Graph = ({ range, f }) =>
+  <div className="App">
+    <Quarter />
+    <Quarter />
+    <Quarter />
+    <Quarter>
+      <Formula xCoords={buildRange(range * 2)} f={f} />
+    </Quarter>
+  </div>
+
+const App = () => <Graph range={1000} f={x => x*(x/1000)} />
+
+export default App
