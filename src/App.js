11f77a202237cdfd7f0a843207640e3ce4b061d9
Marco Guaspari Worms <marcogworms@gmail.com>
2016-11-22 23:38:47 -0200

    First commit - Graph background

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div style={{
+          boxSizing: 'border-box',
+          float: 'left',
+          width: '50%',
+          height: '50%',
+          border: 'solid 1px black'
+        }} />
+        <div style={{
+          boxSizing: 'border-box',
+          float: 'left',
+          width: '50%',
+          height: '50%',
+          border: 'solid 1px black'
+        }} />
+        <div style={{
+          boxSizing: 'border-box',
+          float: 'left',
+          width: '50%',
+          height: '50%',
+          border: 'solid 1px black'
+        }} />
+        <div style={{
+          boxSizing: 'border-box',
+          float: 'left',
+          width: '50%',
+          height: '50%',
+          border: 'solid 1px black'
+        }} />
+      </div>
+    );
+  }
+}
+
+export default App;
